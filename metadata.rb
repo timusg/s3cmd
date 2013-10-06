3d3f90cd486e607cfc633d15442339cbb68bf868
sumit <sugupta4@gmail.com>
2013-10-06 19:59:14 +0530

    supported OS sepecifications in metadata

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,6 @@ description      "Installs/Configures s3cmd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
+%w{centos redhat}.each do |os|
+  supports os
+end
